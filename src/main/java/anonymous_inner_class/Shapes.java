ec0bfbcf7cc2d9ce76d8076c91d0c3df3b575c0b
Alex Poma <alex.rpoma@outlook.com>
2023-07-05 11:25:59 -0400

    java doc anonymous class

--- src/main/java/anonymous_inner_class/Shapes.java
+++ src/main/java/anonymous_inner_class/Shapes.java
@@ -2,6 +2,13 @@ package anonymous_inner_class;
 
 import java.util.List;
 
+/**
+ * An anonymous class is a local class that does not have a name.
+ * It allows you to create a class instance and define its implementation
+ * simultaneously, without explicitly declaring a separate class.
+ * Anonymous classes are useful when you need to create a small,
+ * one-time-use class without the need for re-usability.
+ */
 public class Shapes {
   Shape circleShape = new Shape() {
     @Override
